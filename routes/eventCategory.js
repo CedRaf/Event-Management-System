71cdc26ff53425c086de72294ea2a221039956eb
Ced <cedricrafanan27@gmail.com>
2024-11-27 23:18:59 +0800

    Minor changes in retrieving data from req.body to req.params

--- routes/eventCategory.js
+++ routes/eventCategory.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middlewares/authenticateToken");
 const eventCategoryRouter = express.Router(); 
 eventCategoryRouter.post("/create", authMiddleware, eventCategory.createCategory); 
 eventCategoryRouter.get("/find/:category_name", authMiddleware, eventCategory.findCategory);
-eventCategoryRouter.get("/findAll", authMiddleware, eventCategory.getAllCategories);
+eventCategoryRouter.get("/findAll/:userID", authMiddleware, eventCategory.getAllCategories);
 eventCategoryRouter.delete("/delete", authMiddleware, eventCategory.deleteCategory);
 eventCategoryRouter.patch("/edit/:categoryID", authMiddleware, eventCategory.editCategory); 
 eventCategoryRouter.get("/sort/:orderBy", authMiddleware, eventCategory.sortCategories); 
